aed28dc2c3e647161c9096ccc87f3ab6557b23f1
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-17 16:07:30 +0200

    Changed table name

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Item")
+@Entity(tableName = "item")
 data class Item(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
