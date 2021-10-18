6885168c2493ed11973f19bc20b0d7c595626e5b
VitasSalvantes <salvantesvitas@gmail.com>
2021-10-18 18:20:28 +0200

    Added view model and method to bind item and to display it

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -22,8 +22,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
+import com.example.inventory.data.getFormattedPrice
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -32,7 +35,12 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
  */
 class ItemDetailFragment : Fragment() {
     private val navigationArgs: ItemDetailFragmentArgs by navArgs()
-
+    lateinit var item: Item
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -60,11 +68,28 @@ class ItemDetailFragment : Fragment() {
             .show()
     }
 
+    private fun bind(item: Item) {
+        binding.apply {
+            itemName.text = item.name
+            itemPrice.text = item.getFormattedPrice()
+            itemCount.text = item.quantity.toString()
+        }
+    }
+
     /**
      * Deletes the current item and navigates to the list fragment.
      */
     private fun deleteItem() {
         findNavController().navigateUp()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
     }
 
     /**
